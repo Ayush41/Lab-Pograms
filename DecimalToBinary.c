2dc49024ed393fcd7520e8e281d4d28d1cc46d11
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2025-08-27 09:49:42 +0000

    program done dec to bin

--- DecimalToBinary.c
+++ DecimalToBinary.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    printf("Program has been initialized...\n");
+    // printf("Program has been initialized...\n");
     printf("Program to convert Decimal to binary\n");
     
-    int num;
+    int r=0,num;
+    int i=0;
     //taking input
     printf("\nEnter the number in decimal:-\n");
     scanf("%d",&num);
-    
-    
-    
+    printf("\n\n\t\t\t\t");
+
+   while(num>0){
+        r = num%2;
+        num=num/2;
+        printf("   \b\b%d",r);
+        // Sleep(1000);
+    }
     return 0;
 }
--- program1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-
-
-int main(){
-    printf("Program has been initialized...");
-
-    return 0;
-}

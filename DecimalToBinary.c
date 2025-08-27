9107075ad0bef503e5862d19f52584c941b9e5d2
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2025-08-27 09:00:56 +0000

    init program 1

---
+++ DecimalToBinary.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    printf("Program has been initialized...\n");
+    printf("Program to convert Decimal to binary\n");
+    
+    int num;
+    //taking input
+    printf("\nEnter the number in decimal:-\n");
+    scanf("%d",&num);
+    
+    
+    
+    return 0;
+}

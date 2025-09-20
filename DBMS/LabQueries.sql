ff578022583f636e1b052ac257866bdc8516bd12
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2025-09-20 04:03:06 +0000

    6queries added

--- DBMS/LabQueries.sql
+++ DBMS/LabQueries.sql
@@ -29,3 +29,35 @@ HAVING AVG(salary) > 40000;
 SELECT department_id,AVG(salary) AS avg_salary 
 FROM employees
 Group BY department_id HAVING avg(salary) >40000;
+
+-- Q. Count the num of employees in each department.
+SELECT dept_id, COUNT(*) AS employee_count
+FROM emp
+GROUP BY dept_id;
+
+-- Q. Find the total salary paid in each department
+SELECT dept_id, SUM(salary) AS total_salary
+FROM emp
+GROUP BY dept_id;
+
+-- Q. Find the min salary for each department, only showing departments where the min salary is 50000
+SELECT dept_id, MIN(salary) AS min_salary
+FROM emp
+GROUP BY dept_id
+HAVING MIN(salary) = 50000;
+
+-- Q. find departments where the totla salary paid is ebtween 100,000 and 200,000
+SELECT dept_id, SUM(salary) AS total_salary FROM emp
+GROUP BY dept_id
+HAVING SUM(salary) BETWEEN 100000 and 200000;
+
+-- Q. Find the number of employees in each department who joined after 2022
+SELECT department_id, COUNT(*) AS employee_count
+FROM employees
+WHERE hire_date > TO_DATE('31-12-2022', 'DD-MM-YYYY')
+GROUP BY department_id;
+
+
+-- Q. Find departments with at least one employer who has not provided an email
+SELECT distinct department_id FROM employees
+where email is null or email='';

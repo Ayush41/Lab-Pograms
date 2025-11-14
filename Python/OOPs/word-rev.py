fea2f727ab43fffd3a2249ec88242ae9b6f9af7b
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2025-11-14 05:37:28 +0000

    sentence rev

--- Python/OOPs/word-rev.py
+++ Python/OOPs/word-rev.py
@@ -1,4 +1,16 @@
 
 ''' 
-write a program reverse of the word/sentence
+write a program reverse of the word/sentence but maintain the same sequence of words.
 '''
+
+def rev_word(sentence):
+    return sentence[::-1]
+
+if __name__ == "__main__":
+    # input_sentence = "Hello World"
+
+    # user input
+    input_sentence = input("Enter a sentence to reverse: ")
+    reversed_sentence = rev_word(input_sentence)
+    print(f"Original Sentence: {input_sentence}")
+    print(f"Reversed Sentence: {reversed_sentence}")

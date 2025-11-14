ebd23593de981ae4172537ec0a50e0bc956c4aef
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2025-11-14 05:38:29 +0000

    other sol

--- Python/OOPs/word-rev.py
+++ Python/OOPs/word-rev.py
@@ -14,3 +14,18 @@ if __name__ == "__main__":
     reversed_sentence = rev_word(input_sentence)
     print(f"Original Sentence: {input_sentence}")
     print(f"Reversed Sentence: {reversed_sentence}")
+
+
+
+def rev_word(sentence):
+    words = sentence.split()  # Split sentence into words
+    reversed_words = [word[::-1] for word in words]  # Reversing each word
+    return ' '.join(reversed_words)  
+
+if __name__ == "__main__":
+    # User input
+    input_sentence = input("Enter a sentence to reverse the words: ")
+    reversed_sentence = rev_word(input_sentence)
+    
+    print(f"Original Sentence: {input_sentence}")
+    print(f"Sentence with Reversed Words: {reversed_sentence}")

4469d5bbf89324fe239b7debf8d90bdac9b3f117
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2026-01-19 07:03:27 +0000

    mean,mode,stddev

--- Python/AI-Lab(Noob)/statistics.py
+++ Python/AI-Lab(Noob)/statistics.py
@@ -1,19 +1,30 @@
 # 1. python program to calculate mean median mode std dev, variance. (no library)
-def mean(self,data):
+def mean(data):
     return sum(data)/len(data)
 
-def med(self,data):
-     data.sort() #sort data first
+def median(data):
+    data_sorted = sorted(data)  # Create a sorted copy without modifying the original
+    n = len(data_sorted)
+    if n % 2 == 0:
+        median = (data_sorted[n//2 - 1] + data_sorted[n//2]) / 2
+    else:
+        median = data_sorted[n//2]
+    return median
 
-def mode(self,data):
-
-def stddev(self,data):
+def mode(data): # most frequent element in data
+    pass
+    
+    
+def std_dev(data):
+    meanval = mean(data)
+    var = sum((x - meanval) ** 2 for x in data) / len(data)
+    return var ** 0.5
 
 
 if __name__ == "__main__":
-    data = [1, 2, 2, 3, 4]
+    data = [1, 2, 2, 3, 4,5,0,4,3,1]
     print("Mean:", mean(data))
-    print("Median:", med(data))
-    print("Mode:", mode(data))
-    print("Standard Deviation:", stddev(data))
+    print("Median:", median(data))
+    # print("Mode:", mode(data))
+    print("Standard Deviation:", std_dev(data))
     

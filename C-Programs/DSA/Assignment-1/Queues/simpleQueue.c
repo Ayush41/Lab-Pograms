dae147641461a1b531d512868bcb2826bb560b88
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2026-01-27 07:02:47 +0000

    intit

---
+++ C-Programs/DSA/Assignment-1/Queues/simpleQueue.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// what is queues 
+// A queue is a linear data structure that follows the FIFO (First In First Out) principle.
+// Elements are added at the rear and removed from the front.
+
+// Operations in simple queues are ->
+// 1. Enqueue (add element)
+int EnQ(int front,int rear, int size, int queue[], int value){
+    if(rear == size - 1){
+        printf("Queue is full\n");
+        return -1;
+    }
+    rear++;
+    queue[rear] = value;
+    return rear;
+}
+
+// 2. Dequeue (remove element)
+// 3. Front (view front element)
+// 4. Rear (view rear element)
+// 5. isEmpty (check if queue is empty)
+// 6. isFull (check if queue is full)
+
+int main(){
+
+
+}

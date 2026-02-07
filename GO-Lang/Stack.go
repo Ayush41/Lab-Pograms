953fab406c57d6cbbdc04b51ac92b59bb11f62d0
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2026-02-07 06:07:03 +0000

    stack fnc

--- GO-Lang/Stack.go
+++ GO-Lang/Stack.go
@@ -10,34 +10,43 @@ type Stack struct {
 
 // Push adds an element to the top of the stack
 func (s *Stack) Push(item interface{}) {
-
+	s.items = append(s.items, item)
 }
 
 // Pop removes and returns the top element from the stack
 func (s *Stack) Pop() (interface{}, error) {
-
+	if s.IsEmpty() {
+		return nil, fmt.Errorf("stack is empty")
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item, nil
 }
 
 // Peek returns the top element without removing it
 func (s *Stack) Peek() (interface{}, error) {
-
+	if s.IsEmpty() {
+		return nil, fmt.Errorf("stack is empty")
+	}
+	return s.items[len(s.items)-1], nil
 }
 
 // IsEmpty checks if the stack is empty
 func (s *Stack) IsEmpty() bool {
-
+	return len(s.items) == 0
 }
 
 // Size returns the number of elements in the stack
 func (s *Stack) Size() int {
-
+	return len(s.items)
 }
 
 // Clear removes all elements from the stack
 func (s *Stack) Clear() {
-
+	s.items = nil
 }
 
 func main() {
 	stack := &Stack{}
+
 }

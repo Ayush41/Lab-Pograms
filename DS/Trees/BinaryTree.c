52c006ff36e491b5af0621ceabf1e92841449bf5
Ayush Raj <74952106+Ayush41@users.noreply.github.com>
2026-04-25 07:50:32 +0000

    graph docs and temp

--- DS/Trees/BinaryTree.c
+++ DS/Trees/BinaryTree.c
@@ -1,17 +1,187 @@
-// Structure of each node of the tree. 
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of each node of the tree.
 struct Node {
     int data;
     struct Node* left;
     struct Node* right;
-}; 
+};
 
-// Note : Unlike other languages, C does not support 
-// Object Oriented Programming. So we need to write 
-// a separat method for create and instance of tree node
+// Function to create a new node
 struct Node* newNode(int item) {
-    struct Node* temp = 
-       (struct Node*)malloc(sizeof(struct Node));
-    temp->key = item;
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = item;
     temp->left = temp->right = NULL;
     return temp;
+}
+
+// Function to insert a node in BST
+struct Node* insert(struct Node* node, int key) {
+    if (node == NULL) return newNode(key);
+
+    if (key < node->data)
+        node->left = insert(node->left, key);
+    else if (key > node->data)
+        node->right = insert(node->right, key);
+
+    return node;
+}
+
+// Function to do inorder traversal
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+// Function to do preorder traversal
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Function to do postorder traversal
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+// Function to search a key in BST
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (root->data < key)
+        return search(root->right, key);
+
+    return search(root->left, key);
+}
+
+// Function to find the inorder successor
+struct Node* minValueNode(struct Node* node) {
+    struct Node* current = node;
+    while (current && current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+// Function to delete a node
+struct Node* deleteNode(struct Node* root, int key) {
+    if (root == NULL) return root;
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else {
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        struct Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+// Function to count nodes
+int countNodes(struct Node* root) {
+    if (root == NULL) return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+// Function to find height of the tree
+int height(struct Node* node) {
+    if (node == NULL) return 0;
+    else {
+        int lheight = height(node->left);
+        int rheight = height(node->right);
+        if (lheight > rheight)
+            return (lheight + 1);
+        else return (rheight + 1);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int choice, value;
+
+    while (1) {
+        printf("\nBinary Tree Operations:\n");
+        printf("1. Insert\n");
+        printf("2. Inorder Display\n");
+        printf("3. Preorder Display\n");
+        printf("4. Postorder Display\n");
+        printf("5. Search\n");
+        printf("6. Delete\n");
+        printf("7. Count Nodes\n");
+        printf("8. Height\n");
+        printf("9. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+            case 2:
+                printf("Inorder traversal: ");
+                inorder(root);
+                printf("\n");
+                break;
+            case 3:
+                printf("Preorder traversal: ");
+                preorder(root);
+                printf("\n");
+                break;
+            case 4:
+                printf("Postorder traversal: ");
+                postorder(root);
+                printf("\n");
+                break;
+            case 5:
+                printf("Enter value to search: ");
+                scanf("%d", &value);
+                if (search(root, value) != NULL)
+                    printf("Value found\n");
+                else
+                    printf("Value not found\n");
+                break;
+            case 6:
+                printf("Enter value to delete: ");
+                scanf("%d", &value);
+                root = deleteNode(root, value);
+                break;
+            case 7:
+                printf("Number of nodes: %d\n", countNodes(root));
+                break;
+            case 8:
+                printf("Height of tree: %d\n", height(root));
+                break;
+            case 9:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
 }
